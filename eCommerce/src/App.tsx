77e7e231fd6bc8d3ab6936ff030a354bf42b137f
TerebinovSergey <neznacomec94@gmail.com>
2024-05-06 01:02:45 +0400

    feat: install husky and lint-staged

--- eCommerce/src/App.tsx
+++ eCommerce/src/App.tsx
@@ -1,9 +1,15 @@
 import cl from './App.module.css';
 
 function App() {
+  const title: string = 'Hi!';
   return (
     <>
-      <h1 className={cl.header}>Hellow world!</h1>
+      <h1 className={cl.header}>{title}</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus earum magni quae itaque, voluptate officiis
+        veritatis at iste, quis incidunt cupiditate deleniti molestiae tempore totam laudantium quas perspiciatis?
+        Provident, commodi?
+      </p>
     </>
   );
 }
