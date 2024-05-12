21d39c42d7c1de70bd95e3bc8cf2561d451b8638
zyvy <gm.zyvy@gmail.com>
2024-05-12 21:38:35 +0300

    feat: add main pages templates

---
+++ eCommerce/src/pages/main/Main.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/UI/Header";
+
+function Main() {
+    return (
+        <>
+        <Header />
+        <h1>Main page</h1>
+        </>
+    );
+  }
+  
+  export default Main;
---
+++ eCommerce/src/pages/registration/Registration.tsx
@@ -0,0 +1,7 @@
+function Registration() {
+    return (
+      <h1>Registration page</h1>
+    );
+  }
+  
+  export default Registration;
