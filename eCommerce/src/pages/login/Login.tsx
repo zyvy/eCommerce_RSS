65535b08222651d75bea1bdc1165a88afe000014
TerebinovSergey <neznacomec94@gmail.com>
2024-05-19 23:40:11 +0400

    update: change the login page

--- eCommerce/src/pages/login/Login.tsx
+++ eCommerce/src/pages/login/Login.tsx
@@ -34,16 +34,26 @@ function Login() {
   const clearAuthError = () => setAuthError('');
 
   const handlePasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPasswordError(false);
-    setAuthError('');
-    setPassword(e.target.value);
+    const currentPassword = e.target.value;
     clearAuthError();
+    setPassword(currentPassword);
+    if (currentPassword && !isPasswordValid(currentPassword)) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
   };
 
   const handleEmailInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailError(false);
-    setEmail(e.target.value);
+    const currentEmail = e.target.value;
+    setEmail(currentEmail);
     clearAuthError();
+
+    if (currentEmail && !isEmailValid(currentEmail)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
   };
 
   async function authorization() {
@@ -64,19 +74,8 @@ function Login() {
 
   const submit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    let error = false;
 
-    if (!isPasswordValid(password)) {
-      setPasswordError(true);
-      error = true;
-    }
-
-    if (!isEmailValid(email)) {
-      setEmailError(true);
-      error = true;
-    }
-
-    if (!error) {
+    if (!passwordError && !emailError) {
       authorization();
     }
   };
@@ -87,59 +86,57 @@ function Login() {
   const emailErrorText = 'Incorrect email. The email should be like: example@email.com';
 
   return (
-    <>
+    <div className={styles.container}>
       <Header />
-      <div className={styles.container}>
-        <form className={styles.form} onSubmit={submit}>
-          <h2 className={styles.title}>Sign in</h2>
-          <TextField
-            // defaultValue="johndoe@example.com"
-            error={emailError}
-            helperText={emailError ? emailErrorText : ''}
-            required
-            id="login_email"
-            label="email"
-            variant="outlined"
-            onInput={handleEmailInput}
-          />
-          <TextField
-            // defaultValue="Secret123"
-            error={passwordError}
-            helperText={passwordError ? passwordErrorText : ''}
-            required
-            id="login_password"
-            label="password"
-            type={showPassword ? 'text' : 'password'}
-            onInput={handlePasswordInput}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end">
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-          {authError.length > 0 && <div className={styles.errorMessage}>{authError}</div>}
-          <Button type="submit" className={styles.button} variant="contained">
-            Sign in
-          </Button>
-          <h4 className={styles.subtitle}>Don&#39;t have an account?</h4>
-          <Button
-            className={[styles.button, styles.buttonNewAccount].join(' ')}
-            variant="outlined"
-            onClick={() => navigate(PagePaths.Register)}>
-            Create new account
-          </Button>
-        </form>
-      </div>
+      <form className={styles.form} onSubmit={submit}>
+        <h2 className={styles.title}>Sign in</h2>
+        <TextField
+          // defaultValue="johndoe@example.com"
+          error={emailError}
+          helperText={emailError ? emailErrorText : ''}
+          required
+          id="login_email"
+          label="email"
+          variant="outlined"
+          onInput={handleEmailInput}
+        />
+        <TextField
+          // defaultValue="Secret123"
+          error={passwordError}
+          helperText={passwordError ? passwordErrorText : ''}
+          required
+          id="login_password"
+          label="password"
+          type={showPassword ? 'text' : 'password'}
+          onInput={handlePasswordInput}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end">
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        {authError.length > 0 && <div className={styles.errorMessage}>{authError}</div>}
+        <Button type="submit" className={styles.button} variant="contained">
+          Sign in
+        </Button>
+        <h4 className={styles.subtitle}>Don&#39;t have an account?</h4>
+        <Button
+          className={[styles.button, styles.buttonNewAccount].join(' ')}
+          variant="outlined"
+          onClick={() => navigate(PagePaths.Register)}>
+          Create new account
+        </Button>
+      </form>
       <Footer />
-    </>
+    </div>
   );
 }
 
