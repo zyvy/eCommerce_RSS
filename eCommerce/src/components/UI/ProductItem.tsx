b212972bdb2363435dfe9aa9bfa481c5ff1c0e69
Sergey Kruglov <serega-kolpino@inbox.ru>
2024-05-22 23:38:18 +0300

    feat: start product item

---
+++ eCommerce/src/components/UI/ProductItem.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './footer';
+
+function ProductItem() {
+  
+
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default ProductItem;
