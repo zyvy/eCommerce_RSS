193ede26e650a06877f03ef8495de49dd05e0b4e
zyvy <gm.zyvy@gmail.com>
2024-05-29 19:24:52 +0300

    feat: add page for catalog

---
+++ eCommerce/src/pages/catalog/Catalog.tsx
@@ -0,0 +1,17 @@
+import Footer from "../../components/UI/footer/Footer";
+import Header from "../../components/UI/header/Header";
+import ProductList from "../../components/UI/products/products";
+
+function Catalog() {
+  
+
+  return (
+    <>
+      <Header />
+      <ProductList />
+      <Footer />
+    </>
+  );
+}
+
+export default Catalog;
